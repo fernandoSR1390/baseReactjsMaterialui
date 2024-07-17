1a42ecfedcc80961ff7e79006139b7647f4dc47c
@fernandosalinasromero04 <fernandosalinasromero04@gmail.com>
2024-07-17 11:52:53 -0400

    algunas pruebas material UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import {
+  Box
+} from "@mui/material";
+import "./App.css";
+import DialogComponent from "./DialogComponent";
+import CardComponent from "./CardComponent";
 
 function App() {
+
   return (
-    <div className="App">
-      <h1 className="">PROYECTO BASE REACTJS - MATERIALUI</h1>
-    </div>
+    <Box className="App">
+      <div>
+        <ul className="CompanyInfo">
+          <li className="Name">Company Name</li>
+          <li className="Production">Production</li>
+        </ul>
+      </div>
+      <DialogComponent />
+      <CardComponent />
+    </Box>
   );
 }
 
